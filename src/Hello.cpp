8b1544aa1910ba1011605735b5426ce6d70b7f6a
David Holmes <dholmes@dholmes.us>
2018-10-11 20:33:28 -0500

    Add fmt::print()

--- src/Hello.cpp
+++ src/Hello.cpp
@@ -1,7 +1,14 @@
+#include <fmt/core.h>
+
 #include <iostream>
 
 int main(int /*argc*/, char ** /*argv*/)
 {
-    std::cout << "Hello, World!\n";
+    const char *const iostreamsStr = "iostreams";
+    std::cout << "Hello, World using " << iostreamsStr << "!\n";
+
+    const char *const fmtStr = "fmt";
+    fmt::print("Hello, World using {}!\n", fmtStr);
+
     return 0;
 }
